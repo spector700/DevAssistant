94bc9330da397d2f125d13d4f8442f5d6a45edfe
spector700 <72362399+spector700@users.noreply.github.com>
2023-12-12 20:12:27 -0600

    Added check_file function and some cleanup

--- dev-assistant.py
+++ dev-assistant.py
@@ -5,24 +5,28 @@ from rich.theme import Theme
 from rich.console import Console
 
 # Name of the new project
-project = sys.argv[1]
+PROJECT = sys.argv[1]
 # The directory for the projects
-path = f"/home/nick/Projects/{project}"
+PATH = f"/home/nick/Projects/{PROJECT}"
 # Avaliable languages
-lang_options = ["Python"]
+LANG_OPTIONS = ["Python"]
 
 print_theme = Theme({"exists": "green", "creating": "dark_orange3"})
 console = Console(theme=print_theme)
 
 
 def main():
-    lang = select_lang(lang_options)
+    lang = select_lang(LANG_OPTIONS)
     # Create the new project folder
-    create_dir(lang)
+    create_dir()
+    # Check or Download files
+    check_file(file="flake.nix", lang=lang, download=True)
+    check_file(file=".envrc")
     # Ask to create git repo
     while True:
-        ans = input("Do you want to create a github repo? (y | n)\n").lower()
+        ans = input("Do you want to create a github repo? (Y | N) --> ").lower()
         if ans == "y":
+            check_file(file=".gitignore", lang=lang, download=True)
             create_repo()
             break
         elif ans == "n":
@@ -36,56 +40,46 @@ def select_lang(lang_options) -> str:
         for i in range(len(lang_options)):
             print(f"{i + 1}. {lang_options[i]}\n")
         try:
-            lang = lang_options[int(input("Select a number. \n")) - 1]
+            lang = lang_options[int(input("Select a number. --> ")) - 1]
         except (ValueError, IndexError):
             print("Enter a correct number\n")
     return lang
 
 
 # Create and check for project directory and files
-def create_dir(lang: str):
+def create_dir():
     try:
-        os.mkdir(path)
-        print(f"\nI created the folder {project}")
+        os.mkdir(PATH)
+        print(f"\nI created the folder {PROJECT}")
     except FileExistsError:
-        print(f"\nThe folder {project} already exists...")
+        print(f"\nThe folder {PROJECT} already exists...")
 
-    os.chdir(path)
+    os.chdir(PATH)
 
-    # Check for flake
-    full_path = os.path.join(path, "flake.nix")
+
+def check_file(file, lang=None, download=False):
+    full_path = os.path.join(PATH, file)
     if os.path.isfile(full_path):
-        console.print("\nflake.nix exists", style="exists")
+        console.print(f"{file} exists", style="exists")
+        return
     else:
-        console.print("Downloading flake.nix", style="creating")
-        os.system(
-            f"wget https://raw.githubusercontent.com/spector700/Templates/main/{lang}/flake.nix"
-        )
-
-    # Check for .envrc
-    full_path = os.path.join(path, ".envrc")
-    if os.path.isfile(full_path):
-        console.print(".envrc exists", style="exists")
-    else:
-        console.print("Creating .envrc...", style="creating")
-        with open(".envrc", "w") as direnv:
-            direnv.write("use flake")
-
-    # Check for .gitignore
-    full_path = os.path.join(path, ".gitignore")
-    if os.path.isfile(full_path):
-        console.print(".gitignore exists\n", style="exists")
-    else:
-        console.print("Downloading .gitignore", style="creating")
-        os.system(
-            f"wget https://raw.githubusercontent.com/spector700/Templates/main/{lang}/.gitignore"
-        )
+        # Download from the Templates repo
+        if download:
+            console.print(f"Downloading {file}", style="creating")
+            os.system(
+                f"wget https://raw.githubusercontent.com/spector700/Templates/main/{lang}/{file}"
+            )
+        # Create the .envrc
+        else:
+            console.print("Creating .envrc...", style="creating")
+            with open(".envrc", "w") as direnv:
+                direnv.write("use flake")
 
 
 # Creates the repo and the github remote repo
 def create_repo():
     os.system("git init -b main")
-    os.system(f"gh repo create {project} --add-readme --public --source=.")
+    os.system(f"gh repo create {PROJECT} --add-readme --public --source=.")
     os.system("git add . && git commit -m 'initial commit'")
     os.system("git push --set-upstream origin main")
 
