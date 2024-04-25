60a70e4455ab178b5de117bb4c66824c234f320e
spector700 <72362399+spector700@users.noreply.github.com>
2024-04-25 15:10:16 -0500

    flake update

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       # Systems supported
       allSystems = [
@@ -16,38 +17,38 @@
       ];
 
       # Helper to provide system-specific attributes
-      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs allSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
     in
     {
-      formatter = forAllSystems ({ pkgs }: { default = pkgs.nixpkgs-fmt; });
-
       # Development environment output
-      devShells = forAllSystems ({ pkgs }: {
-        default =
-          pkgs.mkShell {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
             # The Nix packages provided in the environment
             packages = with pkgs; [
               gh
               # Python plus helper tools
-              (python311.withPackages (ps: with ps; [
-                rich
-              ]))
+              (python311.withPackages (ps: with ps; [ rich ]))
             ];
 
             shellHook = ''
               echo "welcome to python" | ${pkgs.lolcat}/bin/lolcat
             '';
           };
-      });
+        }
+      );
 
-      packages = forAllSystems ({ pkgs }: { default = pkgs.callPackage ./. { }; });
+      packages = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.callPackage ./. { };
+        }
+      );
 
       homeManagerModules = {
-        dev-assistant = import ./module.nix {
-          inherit self;
-        };
+        dev-assistant = import ./module.nix { inherit self; };
         default = self.homeManagerModules.dev-assistant;
       };
     };
